eed0192883f53dfc3b4a79769665af0a344986e2
Dishant <dyno3ddd@gmail.com>
2024-08-07 20:23:15 +0530

    UTM parameters passed to form submission

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,99 +1,231 @@
-"use client"
+"use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useFormspark } from "@formspark/use-formspark";
 
 const FORMSPARK_FORM_ID = "ta0rBdRwE";
 
 export default function FormComponent() {
+   let [active_spinner, setActiveSpinner] = useState(false);
 
-    let [active_spinner, setActiveSpinner] = useState(false);
+   const [utmParams, setUtmParams] = useState({
+      utmSource: "",
+      utmName: "",
+   });
 
-    const [submit, submitting] = useFormspark({
-        formId: FORMSPARK_FORM_ID,
-    });
+   const [submit, submitting] = useFormspark({
+      formId: FORMSPARK_FORM_ID,
+   });
 
-    const [FirstName, setFirstName] = useState("");
-    const [LastName, setLastName] = useState("");
-    const [MobileNumber, setMobileNumber] = useState("");
-    const [Service, setService] = useState("");
-    const [City, setCity] = useState("");
-    const [TravelledBefore, setTravelledBefore] = useState("");
+   const [FirstName, setFirstName] = useState("");
+   const [LastName, setLastName] = useState("");
+   const [MobileNumber, setMobileNumber] = useState("");
+   const [Service, setService] = useState("");
+   const [City, setCity] = useState("");
+   const [TravelledBefore, setTravelledBefore] = useState("");
 
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setActiveSpinner(!active_spinner)
-        await submit({
-            FirstName,
-            LastName,
-            MobileNumber,
-            Service,
-            City,
-            TravelledBefore,
-        });
-        window.location.href = "/thank-you";
-    };
+   useEffect(() => {
+      const utmParams = {
+         utmSource: localStorage.getItem("utmSource") || "",
+         utmName: localStorage.getItem("utmName") || "",
+      };
+      setUtmParams(utmParams);
 
+      // console.log(utmParams);
+   }, []);
 
-    return <div className="desktop:w-[1700px] mx-auto px-4 md:px-8 lg:px-4 lg:w-[980px] 2xl:w-[1250px]">
+   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setActiveSpinner(!active_spinner);
+      await submit({
+         FirstName,
+         LastName,
+         MobileNumber,
+         Service,
+         City,
+         TravelledBefore,
+         ...utmParams,
+      });
+      window.location.href = "/thank-you";
+   };
 
-        <h1 className="my-10 lg:my-20 mb-6 font-bold text-2xl text-center lg:text-3xl">Submit your details</h1>
+   return (
+      <div className="desktop:w-[1700px] mx-auto px-4 md:px-8 lg:px-4 lg:w-[980px] 2xl:w-[1250px]">
+         <h1 className="my-10 lg:my-20 mb-6 font-bold text-2xl text-center lg:text-3xl">
+            Submit your details
+         </h1>
 
-        <form onSubmit={onSubmit} className="relative font-bold text-base">
+         <form
+            onSubmit={onSubmit}
+            data-formtrack
+            data-formtrack-params="utm_source, referrer,ref "
+            className="relative font-bold text-base"
+         >
             <div className="relative flex justify-between my-6 w-full">
-                <div className="relative w-[49%]">
-                    <label htmlFor="First Name" className="block w-full">First Name*</label>
-                    <input type="text" name="Name" value={FirstName} onChange={(e) => setFirstName(e.target.value)} id="default-search" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" placeholder="Ayush" required />
-                </div>
-                <div className="relative w-[49%]">
-                    <label htmlFor="Last Name" className="block w-full">Last Name*</label>
-                    <input name="Last Name" value={LastName} onChange={(e) => setLastName(e.target.value)} type="text" id="default-search" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" placeholder="Gupta" required />
-                </div>
+               <div className="relative w-[49%]">
+                  <label
+                     htmlFor="First Name"
+                     className="block w-full"
+                  >
+                     First Name*
+                  </label>
+                  <input
+                     type="text"
+                     name="Name"
+                     value={FirstName}
+                     onChange={(e) => setFirstName(e.target.value)}
+                     id="default-search"
+                     className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                     placeholder="Ayush"
+                     required
+                  />
+               </div>
+               <div className="relative w-[49%]">
+                  <label
+                     htmlFor="Last Name"
+                     className="block w-full"
+                  >
+                     Last Name*
+                  </label>
+                  <input
+                     name="Last Name"
+                     value={LastName}
+                     onChange={(e) => setLastName(e.target.value)}
+                     type="text"
+                     id="default-search"
+                     className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                     placeholder="Gupta"
+                     required
+                  />
+               </div>
             </div>
             <div className="relative my-6">
-                <label htmlFor="MobileNumber" className="block w-full">Mobile Number*</label>
-                <input type="number" name="MobileNumber" value={MobileNumber} onChange={(e) => setMobileNumber(e.target.value)} id="default-search" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" placeholder="92585XXXXX" required />
+               <label
+                  htmlFor="MobileNumber"
+                  className="block w-full"
+               >
+                  Mobile Number*
+               </label>
+               <input
+                  type="number"
+                  name="MobileNumber"
+                  value={MobileNumber}
+                  onChange={(e) => setMobileNumber(e.target.value)}
+                  id="default-search"
+                  className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                  placeholder="92585XXXXX"
+                  required
+               />
             </div>
             <div className="block my-6 w-full">
-                <label htmlFor="Service" className="block w-full">Service interested in:*</label>
-                <select name="Service" value={Service} onChange={(e) => setService(e.target.value)} id="countries" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" required>
-                    <option className="font-semibold text-base" value="" disabled> - select - </option>
-                    <option className="font-semibold text-base" value="male">Full Visa Application</option>
-                    <option className="font-semibold text-base" value="female">Only Early Appointment</option>
-                </select>
+               <label
+                  htmlFor="Service"
+                  className="block w-full"
+               >
+                  Service interested in:*
+               </label>
+               <select
+                  name="Service"
+                  value={Service}
+                  onChange={(e) => setService(e.target.value)}
+                  id="countries"
+                  className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 h-10  border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                  required
+               >
+                  <option
+                     className="font-semibold text-base"
+                     value=""
+                     disabled
+                  >
+                     {" "}
+                     - select -{" "}
+                  </option>
+                  <option
+                     className="font-semibold text-base"
+                     value="Full Visa Application"
+                  >
+                     Full Visa Application
+                  </option>
+                  <option
+                     className="font-semibold text-base"
+                     value="Only Early Appointment"
+                  >
+                     Only Early Appointment
+                  </option>
+               </select>
             </div>
             <div className="relative my-6">
-                <label htmlFor="City" className="block w-full">City*</label>
-                <input name="City" value={City} onChange={(e) => setCity(e.target.value)} type="text" id="default-search" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" placeholder="New Delhi" required />
+               <label
+                  htmlFor="City"
+                  className="block w-full"
+               >
+                  City*
+               </label>
+               <input
+                  name="City"
+                  value={City}
+                  onChange={(e) => setCity(e.target.value)}
+                  type="text"
+                  id="default-search"
+                  className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                  placeholder="New Delhi"
+                  required
+               />
             </div>
             <div className="block my-6 w-full">
-                <label htmlFor="countries" className="block w-full">Have you ever travelled outside of India?*</label>
-                <select name="Travelled Before" value={TravelledBefore} onChange={(e) => setTravelledBefore(e.target.value)} id="countries" className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4 py-2 border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none" required>
-                    <option className="font-semibold text-base" value="" disabled>- select -</option>
-                    <option className="font-semibold text-base" value="Yes">Yes</option>
-                    <option className="font-semibold text-base" value="No">No</option>
-                </select>
+               <label
+                  htmlFor="countries"
+                  className="block w-full"
+               >
+                  Have you ever travelled outside of India?*
+               </label>
+               <select
+                  name="Travelled Before"
+                  value={TravelledBefore}
+                  onChange={(e) => setTravelledBefore(e.target.value)}
+                  id="countries"
+                  className="block border-gray-300 focus:border-2 bg-gray-100 shadow-xs px-4  h-10  border focus:border-blue-400 rounded-lg w-full font-semibold text-gray-900 transition duration-400 placeholder-gray-400 focus:outline-none"
+                  required
+               >
+                  <option
+                     className="font-semibold text-base"
+                     value=""
+                     disabled
+                  >
+                     - select -
+                  </option>
+                  <option
+                     className="font-semibold text-base"
+                     value="Yes"
+                  >
+                     Yes
+                  </option>
+                  <option
+                     className="font-semibold text-base"
+                     value="No"
+                  >
+                     No
+                  </option>
+               </select>
             </div>
 
-            <button type="submit" disabled={submitting} className="flex justify-center items-center bg-[#EB560D] hover:bg-[#141B52] shadow shadow-orange-500 py-3 rounded-lg w-full font-semibold text-lg text-white transition duration-300">
-                <h1 className="pr-3">Submit </h1>
+            <button
+               type="submit"
+               disabled={submitting}
+               className="flex justify-center items-center bg-[#EB560D] hover:bg-[#141B52] shadow shadow-orange-500 py-3 rounded-lg w-full font-semibold text-lg text-white transition duration-300"
+            >
+               <h1 className="pr-3">Submit </h1>
 
-                {active_spinner ? <div
-                    className="inline-block border-2 border-current border-r-transparent border-solid rounded-full w-6 h-6 animate-spin motion-reduce:animate-[spin_1.5s_linear_infinite] align-[-0.125em]"
-                    role="status">
-                </div> : <></>}
-
+               {active_spinner ? (
+                  <div
+                     className="inline-block border-2 border-current border-r-transparent border-solid rounded-full w-6 h-6 animate-spin motion-reduce:animate-[spin_1.5s_linear_infinite] align-[-0.125em]"
+                     role="status"
+                  ></div>
+               ) : (
+                  <></>
+               )}
             </button>
-        </form>
-
-
-    </div>;
+         </form>
+      </div>
+   );
 }
-
-
-
-
-
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,136 @@
+"use client";
+import { useEffect } from "react";
 import AboutUs from "@/Components/AboutUs";
 import FAQ from "@/Components/FAQ";
 import Hero from "@/Components/Hero";
 import Testimonials from "@/Components/Testimonials";
 import { IoCheckmarkCircle } from "react-icons/io5";
 
+type UTMParams = {
+   utmSource: string | null;
+   utmName: string | null;
+};
+
 export default function Home() {
-  return (
+   useEffect(() => {
+      const captureUTMParams = () => {
+         const urlParams = new URLSearchParams(window.location.search);
 
-    <main className="desktop:w-[1700px] mx-auto px-4 md:px-8 lg:px-4 lg:w-[980px] 2xl:w-[1250px]">
-      <Hero />
-      <section id="Procedure" className="mt-10 lg:mt-16 mb-14 lg:mb-16">
-        <h1 className="mb-2 font-bold text-center text-xl lg:text-2xl desktop:text-4xl">What&apos;s The Procedure?</h1>
+         const utmParams: UTMParams = {
+            utmSource: urlParams.get("utm_source"),
+            utmName: urlParams.get("utm_name"),
+         };
 
+         // Store UTM parameters in local storage
+         Object.keys(utmParams).forEach((key) => {
+            const value = utmParams[key as keyof UTMParams];
+            if (value) {
+               localStorage.setItem(key, value);
+            }
+         });
+      };
 
+      captureUTMParams();
+   }, []);
 
-        <div className="lg:flex justify-between items-start hidden text-sm">
-          <div className="my-4 lg:my-0 lg:pr-6 lg:w-1/3">
-            <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> Details and documents gathering</h2>
+   useEffect(() => {
+      const logUTMParamsFromLocalStorage = () => {
+         const utmParams: UTMParams = {
+            utmSource: localStorage.getItem("utmSource"),
+            utmName: localStorage.getItem("utmName"),
+         };
+         console.log("UTM New Parameters");
+         // console.log("UTM Params from Local Storage:", utmParams);
+      };
 
-          </div>
+      logUTMParamsFromLocalStorage();
+   }, []);
 
-          <div className="my-4 lg:my-0 lg:w-1/3">
-            <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> DS 160 Form Filling & US Visa Fee payment</h2>
-
-          </div>
-
-          <div className="my-4 lg:my-0 lg:pl-6 lg:w-1/3">
-            <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> Booking the earliest appointment slot</h2>
-
-          </div>
-        </div>
-
-
-
-        <div className="lg:flex justify-between items-start hidden text-sm">
-          <div className="my-4 lg:my-0 lg:pr-6 lg:w-1/3">
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our experienced executive will gather all your details and documents required to apply for US Visa.</p>
-          </div>
-
-          <div className="my-4 lg:my-0 lg:w-1/3">
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our executive will fill the DS 160 form for US Visa application on your behalf. </p>
-          </div>
-
-          <div className="my-4 lg:my-0 lg:pl-6 lg:w-1/3">
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our team of Visa Experts sit through out the day to spot any new or cancelled vacancies and provide you with the earliest appointment schedule</p>
-          </div>
-        </div>
-
-
-
-
-
-        <div className="block lg:hidden text-sm">
-          <div className="my-4 lg:my-0 lg:pr-6 lg:w-1/3">
-            <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> Details and documents gathering</h2>
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our experienced executive will gather all your details and documents required to apply for US Visa.</p>
-          </div>
-
-          <div className="my-4 lg:my-0 lg:w-1/3">
-            <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> DS 160 Form Filling & US Visa Fee payment</h2>
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our executive will fill the DS 160 form for US Visa application on your behalf. </p>
-          </div>
-
-          <div className="my-4 lg:my-0 lg:pl-6 lg:w-1/3">
-            <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl"> <span className="pt-1 pr-1 text-green-500"><IoCheckmarkCircle /></span> Booking the earliest appointment slot</h2>
-            <p className="font-medium text-sm md:text-base desktop:text-xl">Our team of Visa Experts sit through out the day to spot any new or cancelled vacancies and provide you with the earliest appointment schedule</p>
-          </div>
-        </div>
-      </section>
-      <FAQ />
-      <AboutUs />
-      <Testimonials />
-    </main>
-  );
+   return (
+      <main className="desktop:w-[1700px] mx-auto px-4 md:px-8 lg:px-4 lg:w-[980px] 2xl:w-[1250px]">
+         <Hero />
+         <section
+            id="Procedure"
+            className="mt-10 lg:mt-16 mb-14 lg:mb-16"
+         >
+            <h1 className="mb-2 font-bold text-center text-xl lg:text-2xl desktop:text-4xl">
+               What&apos;s The Procedure?
+            </h1>
+            <div className="lg:flex justify-between items-start hidden text-sm">
+               <div className="my-4 lg:my-0 lg:pr-6 lg:w-1/3">
+                  <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     Details and documents gathering
+                  </h2>
+               </div>
+               <div className="my-4 lg:my-0 lg:w-1/3">
+                  <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     DS 160 Form Filling & US Visa Fee payment
+                  </h2>
+               </div>
+               <div className="my-4 lg:my-0 lg:pl-6 lg:w-1/3">
+                  <h2 className="flex items-start my-1 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     Booking the earliest appointment slot
+                  </h2>
+               </div>
+            </div>
+            <div className="block lg:hidden text-sm">
+               <div className="my-4 lg:my-0 lg:pr-6 lg:w-1/3">
+                  <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     Details and documents gathering
+                  </h2>
+                  <p className="font-medium text-sm md:text-base desktop:text-xl">
+                     Our experienced executive will gather all your details and
+                     documents required to apply for US Visa.
+                  </p>
+               </div>
+               <div className="my-4 lg:my-0 lg:w-1/3">
+                  <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     DS 160 Form Filling & US Visa Fee payment
+                  </h2>
+                  <p className="font-medium text-sm md:text-base desktop:text-xl">
+                     Our executive will fill the DS 160 form for US Visa
+                     application on your behalf.{" "}
+                  </p>
+               </div>
+               <div className="my-4 lg:my-0 lg:pl-6 lg:w-1/3">
+                  <h2 className="flex items-start my-2 font-semibold text-base md:text-lg desktop:text-2xl">
+                     {" "}
+                     <span className="pt-1 pr-1 text-green-500">
+                        <IoCheckmarkCircle />
+                     </span>{" "}
+                     Booking the earliest appointment slot
+                  </h2>
+                  <p className="font-medium text-sm md:text-base desktop:text-xl">
+                     Our team of Visa Experts sit through out the day to spot
+                     any new or cancelled vacancies and provide you with the
+                     earliest appointment schedule
+                  </p>
+               </div>
+            </div>
+         </section>
+         <FAQ />
+         <AboutUs />
+         <Testimonials />
+      </main>
+   );
 }
-
-
