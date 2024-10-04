e6f9eaa06b83afb6e123cb84436e9abffca37db6
Dishant <dyno3ddd@gmail.com>
2024-10-04 08:30:50 +0530

    visaontime.org duplicate GTM setup noIndex

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,86 +3,149 @@ import { GiCommercialAirplane } from "react-icons/gi";
 
 export default function Hero() {
     return (
-        <section id="hero" className="relative mt-12 lg:mt-16 mb-20 lg:mb-0 px-2 md:px-36 lg:px-0">
+       <section
+          id="hero"
+          className="relative mt-12 lg:mt-16 mb-20 lg:mb-0 px-2 md:px-36 lg:px-0"
+       >
+          <div className="lg:flex justify-between items-center text-center lg:text-left">
+             <div className="flex justify-start items-center lg:pt-0 lg:pb-0 lg:w-1/2">
+                <div className="space-y-5 lg:space-y-8 desktop:space-y-16 lg:w-[425px] desktop:w-[750px]">
+                   <h1 className="font-extrabold text-[#141B52]  text-[32px] small:text-[35px] lg:text-[39px] desktop:text-5xl desktop:leading-[66px] lg:leading-[46px] small:leading-[42px] leading-[40px] ">
+                      US Visitor Visa Interview Appointment in just 30
+                      Days!&quot;
+                   </h1>
 
-            <div className="lg:flex justify-between items-center text-center lg:text-left">
-                <div className="flex justify-start items-center lg:pt-0 lg:pb-0 lg:w-1/2">
-                    <div className="space-y-5 lg:space-y-8 desktop:space-y-16 lg:w-[425px] desktop:w-[750px]">
-                        <h1 className="font-extrabold text-[#141B52]  text-[32px] small:text-[35px] lg:text-[39px] desktop:text-5xl desktop:leading-[66px] lg:leading-[46px] small:leading-[42px] leading-[40px] ">
-                            US Visitor Visa
-                            appointment in
-                            just 30 Days!
-                        </h1>
+                   <h1 className="font-medium text-sm md:text-lg lg:text-xl desktop:text-3xl ">
+                      VisaOnTime helps you present yourself for the US Visa
+                      Interview quicker than anyone else.
+                   </h1>
 
-                        <h1 className="font-medium text-sm md:text-lg lg:text-xl desktop:text-3xl ">
-                            VisaOnTime helps you present yourself for the US Visa Interview quicker than anyone else.
-                        </h1>
+                   <div className="md:flex justify-between items-center place-items-center content-center place-content-center grid grid-cols-2 pb-2">
+                      <div className="">
+                         <div className="flex -space-x-4 lg:mb-2 mb-1">
+                            <img
+                               className="border-2 border-white rounded-full w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16 "
+                               src="/1.webp"
+                               alt="Stacked rounded avatar"
+                            />
+                            <img
+                               className="border-2 border-white rounded-full w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16"
+                               src="/2.webp"
+                               alt="Stacked rounded avatar"
+                            />
+                            <img
+                               className="border-2 border-white rounded-full  w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16"
+                               src="/3.webp"
+                               alt="Stacked rounded avatar"
+                            />
+                         </div>
 
-                        <div className="md:flex justify-between items-center place-items-center content-center place-content-center grid grid-cols-2 pb-2">
-                            <div className="">
-                                <div className='flex -space-x-4 lg:mb-2 mb-1'>
-                                    <img className='border-2 border-white rounded-full w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16 ' src='/1.webp' alt='Stacked rounded avatar' />
-                                    <img className='border-2 border-white rounded-full w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16' src='/2.webp' alt='Stacked rounded avatar' />
-                                    <img className='border-2 border-white rounded-full  w-10 h-10 lg:w-11 lg:h-11 desktop:w-16 desktop:h-16' src='/3.webp' alt='Stacked rounded avatar' />
-                                </div>
+                         <div className="flex items-center gap-1 text-amber-500 transition-all duration-500">
+                            <svg
+                               className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]"
+                               viewBox="0 0 18 17"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                            >
+                               <path
+                                  d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                                  fill="currentColor"
+                               ></path>
+                            </svg>
 
-                                <div className="flex items-center gap-1 text-amber-500 transition-all duration-500">
-                                    <svg className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z" fill="currentColor"></path>
-                                    </svg>
+                            <svg
+                               className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]"
+                               viewBox="0 0 18 17"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                            >
+                               <path
+                                  d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                                  fill="currentColor"
+                               ></path>
+                            </svg>
 
-                                    <svg className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z" fill="currentColor"></path>
-                                    </svg>
+                            <svg
+                               className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]"
+                               viewBox="0 0 18 17"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                            >
+                               <path
+                                  d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                                  fill="currentColor"
+                               ></path>
+                            </svg>
 
-                                    <svg className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z" fill="currentColor"></path>
-                                    </svg>
+                            <svg
+                               className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]"
+                               viewBox="0 0 18 17"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                            >
+                               <path
+                                  d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                                  fill="currentColor"
+                               ></path>
+                            </svg>
 
-                                    <svg className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z" fill="currentColor"></path>
-                                    </svg>
+                            <svg
+                               className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]"
+                               viewBox="0 0 18 17"
+                               fill="none"
+                               xmlns="http://www.w3.org/2000/svg"
+                            >
+                               <path
+                                  d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                                  fill="currentColor"
+                               ></path>
+                            </svg>
+                         </div>
+                      </div>
 
-                                    <svg className="w-[16px] h-[16px] lg:w-[16px] lg:h-[16px] desktop:w-[25px] desktop:h-[25px]" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z" fill="currentColor"></path>
-                                    </svg>
-                                </div>
-                            </div>
+                      <p className="px-0 md:px-8 -ml-4 mr-10 small:-ml-12 small:mr-12 md:mr-0 md:-ml-0  font-semibold  text-sm lg:text-sm desktop:text-xl text-center ">
+                         250+ Successful Early Appointments and counting
+                      </p>
 
-                            <p className="px-0 md:px-8 -ml-4 mr-10 small:-ml-12 small:mr-12 md:mr-0 md:-ml-0  font-semibold  text-sm lg:text-sm desktop:text-xl text-center ">
-                                250+ Successful Visa Issuance
-                                and counting.
-                            </p>
+                      <div className="col-span-2 mt-6 mb-2 lg:mt-0 lg:mb-0 place-self-center">
+                         <h2 className="font-extrabold text-[#141B52] text-3xl lg:text-3xl desktop:text-4xl">
+                            ₹39,999/-
+                         </h2>
+                         <p className="text-base font-medium text-center">
+                            (all inclusive)
+                         </p>
+                      </div>
+                   </div>
+                   <div className="">
+                      <div className="shadow shadow-orange-500 hover:shadow-[#141B52] mx-2 md:mx-0 rounded-lg font-semibold text-center text-lg desktop:text-2xl text-white group">
+                         <a
+                            href="/next"
+                            className="group-hover:bg-[#141B52] flex justify-center items-center bg-[#EB560D] py-3 rounded-lg transition duration-300"
+                         >
+                            Get Early Appointment
+                            <span className="ml-2 text-xl">
+                               <GiCommercialAirplane />
+                            </span>
+                         </a>
+                      </div>
 
-                            <div className="col-span-2 mt-6 mb-2 lg:mt-0 lg:mb-0 place-self-center">
-                                <h2 className="font-extrabold text-[#141B52] text-3xl lg:text-3xl desktop:text-4xl">
-                                    ₹39,999/-
-                                </h2>
-                                <p className="text-base font-medium text-center">(all inclusive)</p>
-                            </div>
-                        </div>
-
-                        <div className="shadow shadow-orange-500 hover:shadow-[#141B52] mx-2 md:mx-0 rounded-lg font-semibold text-center text-lg desktop:text-2xl text-white group">
-                            <a href="/next" className="group-hover:bg-[#141B52] flex justify-center items-center bg-[#EB560D] py-3 rounded-lg transition duration-300">
-                                Apply US Visa
-                                <span className="ml-2 text-xl">
-                                    <GiCommercialAirplane />
-                                </span>
-                            </a>
-                        </div>
-                    </div>
+                      <p className="text-xs text-center mt-3">
+                         VisaOnTime is not affiliated with the US government or
+                         embassy. We assist in booking early visa interview
+                         appointments only.
+                      </p>
+                   </div>
                 </div>
+             </div>
 
-
-                <div className="relative flex justify-center lg:justify-end pt-10 lg:pt-0 lg:w-1/2">
-                    <img
-                        src="/hero.webp"
-                        alt="Visa On Time Hero"
-                        className="shadow-2xl rounded-[48px] small:w-[82%] small:h-[82%] "
-                    />
-                </div>
-            </div>
-
-        </section>
+             <div className="relative flex justify-center lg:justify-end pt-10 lg:pt-0 lg:w-1/2">
+                <img
+                   src="/hero.webp"
+                   alt="Visa On Time Hero"
+                   className="shadow-2xl rounded-[48px] small:w-[82%] small:h-[82%] "
+                />
+             </div>
+          </div>
+       </section>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,17 @@ import { GoogleTagManager } from '@next/third-parties/google'
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-   title: "USA Visa in just 30 Days! - Apply Today",
+   title: "Early US Visa Appointment Booking - Schedule Today",
    description:
-      "Get US Visa in just 30 Days. The #1 Visa Agency, successfully assisting US Visa aspirants since 2020. Apply your US Visa today!",
+      "VisaOnTime assists in securing early US Visa interview appointments, helping you complete your application process sooner.",
    keywords: "US, USA, Visa On Time, VisaOnTime, Visa Agency",
    authors: [{ name: "Dishant Gusain", url: "https://dishant.info/" }],
    openGraph: {
       type: "website",
-      title: "USA Visa in just 30 Days! - Apply Today",
+      title: "Early US Visa Appointment Booking - Schedule Today",
       url: "https://visaontime.co/",
       description:
-         "Get US Visa in just 30 Days. The #1 Visa Agency, successfully assisting US Visa aspirants since 2020. Apply your US Visa today!",
+         "VisaOnTime assists in securing early US Visa interview appointments, helping you complete your application process sooner.",
       images: [
          {
             url: "https://firebasestorage.googleapis.com/v0/b/dishant-portfolio-nextjs.appspot.com/o/VisaOnTime.webp?alt=media&token=8257b6a3-ca43-4ad7-9ee4-9a8f94d56240",
@@ -30,48 +30,48 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: Readonly<{
-  children: React.ReactNode;
+   children: React.ReactNode;
 }>) {
-  return (
-     <html lang="en">
-        <head>
-           {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CNZL3ZYY3E"></Script> */}
+   return (
+      <html lang="en">
+         <head>
+            {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CNZL3ZYY3E"></Script> */}
 
-           <meta
-              name="robots"
-              content="noindex, nofollow"
-           />
-           <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-           >
-              {` window.dataLayer = window.dataLayer || [];
+            <meta
+               name="robots"
+               content="noindex, nofollow"
+            />
+            <Script
+               id="google-analytics"
+               strategy="afterInteractive"
+            >
+               {` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-4YQ2N5SVJT');`}
-           </Script>
-        </head>
+            </Script>
+         </head>
 
-        <body className={montserrat.className}>
-           <Navbar />
+         <body className={montserrat.className}>
+            <Navbar />
 
-           <GoogleTagManager gtmId="GTM-T674TVZN" />
+            <GoogleTagManager gtmId="GTM-TP2WQDLT" />
 
-           {children}
-           <span
-              itemProp="thumbnail"
-              itemScope
-              itemType="http://schema.org/ImageObject"
-           >
-              <link
-                 itemProp="url"
-                 href="https://firebasestorage.googleapis.com/v0/b/dishant-portfolio-nextjs.appspot.com/o/VisaOnTime.webp?alt=media&token=8257b6a3-ca43-4ad7-9ee4-9a8f94d56240"
-              />
-           </span>
-           <Footer />
-        </body>
-     </html>
-  );
+            {children}
+            <span
+               itemProp="thumbnail"
+               itemScope
+               itemType="http://schema.org/ImageObject"
+            >
+               <link
+                  itemProp="url"
+                  href="https://firebasestorage.googleapis.com/v0/b/dishant-portfolio-nextjs.appspot.com/o/VisaOnTime.webp?alt=media&token=8257b6a3-ca43-4ad7-9ee4-9a8f94d56240"
+               />
+            </span>
+            <Footer />
+         </body>
+      </html>
+   );
 }
